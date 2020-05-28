72f76e38d5ba9966c09024c357a6aa46d5716983
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-05-28 16:06:15 +0200

    test async

--- src/store/action.test.js
+++ src/store/action.test.js
@@ -5,13 +5,14 @@ describe('actions', () => {
   describe('logout', () => {
     test('should create a USER_LOGOUT action', () => {
       const expectedAction = {
-        type: TYPES.USER_LOGOUT
+        type: TYPES.USER_LOGOUT,
       };
       expect(actions.logout()).toEqual(
         expectedAction
       );
     });
   });
+
   describe('saveUserData', () => {
     test('should create a SET_USER action with userName and userPassword', () => {
       const userName = 'Ana';
@@ -19,12 +20,72 @@ describe('actions', () => {
       const expectedAction = {
         type: TYPES.SET_USER,
         userName,
-        userPassword
+        userPassword,
       };
       expect(actions.saveUserData(userName, userPassword)).toEqual(
         expectedAction
       );
     });
   });
-})
+});
+
+describe('fetchRegister', () => {
+  const dispatch = jest.fn();
+  const userName = 'Ana';
+  const userPassword = '1234';
+  const getState = () => { };
+  const action = actions.fetchRegister(userName, userPassword);
+
+  test('should dispatch FETCH_REGISTER_REQUEST and FETCH_REGISTER_SUCCESS action', async () => {
+    const isRegister = { success: true };
+    const ServiceCls = {
+      register: jest.fn().mockReturnValueOnce(isRegister)
+    };
+
+    await action(dispatch, getState, { ServiceCls });
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: TYPES.FETCH_REGISTER_REQUEST,
+    });
+
+    expect(ServiceCls.register).toHaveBeenCalled();
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: TYPES.FETCH_REGISTER_SUCCESS,
+      userName,
+      userPassword
+    });
+  });
+
+  test('should dispatch FETCH_REGISTER_REQUEST and FETCH_REGISTER_FAILURE action', async () => {
+    const error = 'error';
+    const isRegister = { success: false, error: 'success.error' };
+    const ServiceCls = {
+      register: jest.fn().mockReturnValueOnce(isRegister).mockRejectedValue(error)
+    };
+
+    await action(dispatch, getState, { ServiceCls });
+
+    expect(dispatch).toHaveBeenNthCalledWith(1, {
+      type: TYPES.FETCH_REGISTER_REQUEST,
+    });
+
+    expect(ServiceCls.register).toHaveBeenCalled();
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: TYPES.FETCH_REGISTER_FAILURE,
+      errorRegister: 'success.error'
+    });
+
+    await action(dispatch, getState, { ServiceCls });
+
+    expect(dispatch).toHaveBeenCalledWith({
+      type: TYPES.FETCH_REGISTER_FAILURE,
+      errorRegister: 'error'
+    });
+  });
+});
+
+
+
 
