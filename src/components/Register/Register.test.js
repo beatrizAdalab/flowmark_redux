7d3031e2306f2bb1841aaa9baed39bd0cec603cb
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-05-30 19:01:43 +0200

    add register test

--- src/components/Register/Register.test.js
+++ src/components/Register/Register.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Register from './Register';
+import { element } from 'prop-types';
 
 describe('Register', () => {
   const props = {
@@ -17,6 +18,7 @@ describe('Register', () => {
       isFetching: false
     },
     logoutUser: jest.fn(),
+    registerUser: jest.fn((userName, userPassword) => { }),
   };
 
   let wrapper;
@@ -27,5 +29,13 @@ describe('Register', () => {
 
   test('should render', () => {
     expect(wrapper.exists()).toBe(true);
+  });
+
+  test('should register user with userName and userPassword', () => {
+    const e = { preventDefault: jest.fn() }
+    const value = { userName: 'Ana', userPassword: '1234' }
+
+    wrapper.find('Form').props().formSubmit(e, value)
+    expect(props.registerUser).toHaveBeenCalledWith("Ana", "1234");
   });
 });
