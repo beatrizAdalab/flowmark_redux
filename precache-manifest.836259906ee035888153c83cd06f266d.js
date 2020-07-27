2176939186b02ecb757f6ab38f5e5bd4a509e2cd
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-07-27 11:12:04 +0200

    Updates

---
+++ precache-manifest.836259906ee035888153c83cd06f266d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0a66ea0e57dd50e25687a0a59130f8",
+    "url": "/flowmark_redux/index.html"
+  },
+  {
+    "revision": "af65723e33a0fc94780c",
+    "url": "/flowmark_redux/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "357c5699000ba904b4a7",
+    "url": "/flowmark_redux/static/css/main.1213e427.chunk.css"
+  },
+  {
+    "revision": "af65723e33a0fc94780c",
+    "url": "/flowmark_redux/static/js/2.ac551bd5.chunk.js"
+  },
+  {
+    "revision": "acbac6ab9b8e0fae0cfd247b816b11ff",
+    "url": "/flowmark_redux/static/js/2.ac551bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "357c5699000ba904b4a7",
+    "url": "/flowmark_redux/static/js/main.4d7987ba.chunk.js"
+  },
+  {
+    "revision": "11c00a7ac85ce511308b",
+    "url": "/flowmark_redux/static/js/runtime-main.f5b95c4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/beatrizAdalab/flowmark_redux/precache-manifest.0e3736e73812dedb1416607642062d9d.js"
+  "/flowmark_redux/precache-manifest.836259906ee035888153c83cd06f266d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beatrizAdalab/flowmark_redux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flowmark_redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
