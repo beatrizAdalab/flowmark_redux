04c8ffa5c307f5b3ee27416fc4143add2f4bbb3d
beatrizGJ <beatrizgarcia.jim@gmail.com>
2020-07-27 10:40:50 +0200

    public

---
+++ docs/precache-manifest.2258cc2f46e76be3d21ad59b01ec2175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "794a683355f77d6685c004924cd6c0a8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af65723e33a0fc94780c",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "87832832e56654619ad3",
+    "url": "./static/css/main.1213e427.chunk.css"
+  },
+  {
+    "revision": "af65723e33a0fc94780c",
+    "url": "./static/js/2.ac551bd5.chunk.js"
+  },
+  {
+    "revision": "acbac6ab9b8e0fae0cfd247b816b11ff",
+    "url": "./static/js/2.ac551bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87832832e56654619ad3",
+    "url": "./static/js/main.45ad7f68.chunk.js"
+  },
+  {
+    "revision": "0673a29ae19d39d2e1b6",
+    "url": "./static/js/runtime-main.51cd7f76.js"
+  }
+]);
